f9731c644e26af071b1c2e3b63af1519c3d1995d
rossyvee <roseakinyi001@gmail.com>
2022-05-30 00:17:35 +0300

    connecting the daabase, adding boostrap and crispy

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news',
-    'gallery'
+    'gallery',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -128,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_DIRS=[os.path.join(BASE_DIR,'staticfiles')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
