1082fd73ca5286b996e6d22b7d82e874e9a99de5
rossyvee <roseakinyi001@gmail.com>
2022-05-30 00:08:50 +0300

    adding the media url, static files and time zone and importing os

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('uploadphoto/', views.upload_photo, name='upload_photo'),
+    path('uploadphoto/', views.save_image, name='upload_photo'),
     path('category/', views.category, name='category'),
     path('location/', views.location, name='location'),
+    path('photos/', views.photos, name='photos'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
